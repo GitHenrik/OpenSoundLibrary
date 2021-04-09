17857d6887c852367648e7b8d48d4c11e65af026
Robert Kuhlmann <robert.kuhlmann2@gmail.com>
2021-04-09 21:23:18 +0300

    feat: responsive header

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,19 +1,69 @@
 import React from 'react'
 import '../css/base.css'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompactDisc } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import PropTypes from 'prop-types'
+
+const Wrapper = styled.div`
+  background-color: #111111;
+  width: 100%;
+  height: 5.1875rem;
+`
+
+const Header = styled.header`
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  background: #111111;
+  position: sticky;
+  top: 0;
+  padding: 1rem;
+  align-items: center;
+  @media (min-width: 768px) {
+    width: 768px;
+    margin: auto
+  }
+`
+
+const Brand = styled.h1`
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 33px;
+  letter-spacing: -0.05em;
+  color: #FFFFFF;
+  margin: 0;
+`
+
+const IconButton = styled.button`
+  background: none;
+  border: none;
+  display: ${props => props.hideOnMobile ? 'none' : 'block'};
+  @media (min-width: 768px) {
+    display: block;
+  }
+`
+
+IconButton.propTypes = {
+  hideOnMobile: PropTypes.bool
+}
 
 const MainHeader = () => {
   return (
-    <div className="main-header horizontal-container">
-      <div className="centered-item">
-      <i className="fas fa-compact-disc header-icon"></i>
-      </div>
-      <div className="centered-item">
-        <h3>Open Sound Lib</h3>
-      </div>
-      <div className="centered-item">
-      <i className="far fa-user header-icon"></i>
-      </div>
-    </div>
+    <Wrapper>
+      <Header>
+        <IconButton hideOnMobile>
+          <FontAwesomeIcon icon={faCompactDisc} />
+        </IconButton>
+        <Brand>Open Sound Lib</Brand>
+        <IconButton>
+          <FontAwesomeIcon size="lg" icon={faUser} />
+        </IconButton>
+      </Header>
+    </Wrapper>
   )
 }
 
