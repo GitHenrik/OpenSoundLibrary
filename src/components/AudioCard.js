45c08971d53e82c0aedd2418cd13ee8fea4de79b
Henrik <henrik.tarnanen@gmail.com>
2021-04-13 23:09:48 +0300

    feat: added grid view to song listing, later to be toggled by UI component

--- src/components/AudioCard.js
+++ src/components/AudioCard.js
@@ -2,9 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCompactDisc } from '@fortawesome/free-solid-svg-icons'
+import { faDownload, faHeadphones, faHeart, faPlay } from '@fortawesome/free-solid-svg-icons'
 
-const AudioCardContainer = styled.div`
+const AudioCardListContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -12,19 +12,37 @@ const AudioCardContainer = styled.div`
     flex-direction: row;
     height: 241px;
     width: 768px;
+    border-right: 4px solid #eaf27c;
   }
   background: #111111;
   margin-bottom: 1.5rem;
-  border-right: 4px solid #eaf27c;
 `
 
-const PictureContainer = styled.img`
+const AudioCardGridContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: #111111;
+  margin-bottom: 1.5rem;
+`
+
+const ListPictureContainer = styled.img`
   width: 100%;
   height: 100vmin;
   object-fit: cover;
   @media (min-width: 768px) {
     width: 241px;
     height: 241px;
+  }
+`
+
+const GridPictureContainer = styled.img`
+  width: 100%;
+  height: 100vmin;
+  object-fit: cover;
+  @media (min-width: 768px) {
+    width: 228px;
+    height: 228px;
   }
 `
 
@@ -39,19 +57,96 @@ const GenreContainer = styled.div`
   flex-direction: row;
 `
 
-const GenreTag = styled.div`
+const ListGenreTag = styled.div`
   display: inline-block;
   padding: 0 0.75rem;
   background: #eaf27c;
   border-radius: 27.5px;
   font-style: normal;
   font-weight: bold;
-  font-size: 12px;
-  line-height: 16px;
+  font-size: 0.75rem;
+  line-height: 1rem;
   text-align: center;
   letter-spacing: -0.02em;
   color: #000000;
   margin-right: 1rem;
+`
+
+const GridGenreTag = styled.div`
+  display: inline-block;
+  padding: 0 0.75rem;
+  background: #eaf27c;
+  border-radius: 27.5px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 0.75rem;
+  line-height: 1rem;
+  text-align: center;
+  letter-spacing: -0.02em;
+  color: #000000;
+  margin-right: 1rem;
+  @media (min-width: 768px) {
+    margin-right: 0.5rem;
+    padding: 0 0.5rem;
+    font-size: 0.5rem;
+    line-height: 0.6875rem;
+  }
+`
+
+const AudioControls = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: space-between;
+`
+
+const AudioControlButtonContainer = styled.div`
+  text-align: center;
+`
+
+const ListAudioCardBlock = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1.5rem;
+`
+
+const GridAudioCardBlock = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1.5rem;
+  @media (min-width: 768px) {
+    padding: 0.5rem;
+  }
+`
+
+const ListAudioDurationContainer = styled.div`
+  left: 90%;
+  right: 3.97%;
+  top: 10.79%;
+  bottom: 78.84%;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.125rem;
+  line-height: 1.5rem;
+  color: #b0b0b0;
+`
+
+const GridAudioDurationContainer = styled.div`
+  left: 90%;
+  right: 3.97%;
+  top: 10.79%;
+  bottom: 78.84%;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.125rem;
+  line-height: 1.5rem;
+  color: #b0b0b0;
+  @media (min-width: 768px) {
+    display: none;
+  }
 `
 
 const AudioDataContainer = styled.div`
@@ -61,67 +156,109 @@ const AudioDataContainer = styled.div`
   flex-wrap: nowrap;
 `
 
-const AudioData = styled.div`
+const ListAudioData = styled.div`
   flex-direction: row;
   flex-wrap: nowrap;
-  display: inline-block;
-  padding-right: 1rem;
-`
-
-const AudioControls = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  align-items: center;
 `
 
-const AudioControlButtonContainer = styled.div`
-  text-align: center;
-`
-
-const AudioCardBlock = styled.div`
-  display: flex;
-  width: 100%;
+const GridAudioData = styled.div`
   flex-direction: row;
-  justify-content: space-between;
-  padding: 1.5rem;
+  flex-wrap: nowrap;
+  display: flex;
+  align-items: center;
 `
 
-const AudioDurationContainer = styled.div`
-  left: 90%;
-  right: 3.97%;
-  top: 10.79%;
-  bottom: 78.84%;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 25px;
-  color: #b0b0b0;
-`
-
-const AudioDataNumber = styled.div`
-  padding: 0.5rem;
+const ListAudioDataNumber = styled.div`
+  padding: 0.5rem 1.5rem 0.5rem 0.5rem;
   display: inline-block;
+  font-size: 0.875rem;
 `
 
-const SongTitle = styled.div`
+const GridAudioDataNumber = styled.div`
+  padding: 0.5rem 1.5rem 0.5rem 0.5rem;
+  display: inline-block;
+  font-size: 0.875rem;
+  @media (min-width: 768px) {
+    padding: 0.5rem;
+    font-size: 0.625rem;
+  }
+`
+
+const ListSongTitle = styled.div`
   font-family: Open Sans;
   font-style: normal;
   font-weight: normal;
-  font-size: 26px;
-  line-height: 35px;
+  font-size: 1.625rem;
+  line-height: 2.25rem;
   color: #ffffff;
 `
 
-const ArtistName = styled.div`
+const GridSongTitle = styled.div`
   font-family: Open Sans;
   font-style: normal;
   font-weight: normal;
-  font-size: 16px;
-  line-height: 22px;
+  font-size: 1.625rem;
+  line-height: 2.25rem;
+  color: #FFFFFF;
+  @media (min-width: 768px) {
+    font-size: 1rem;
+  line-height: 1.375rem;
+  }
+`
+
+const ListArtistName = styled.div`
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1rem;
+  line-height: 1.375rem;
   color: #b0b0b0;
 `
 
-const AudioCard = ({ audioData }) => {
+const GridArtistName = styled.div`
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1rem;
+  line-height: 1.375rem;
+  @media (min-width: 768px) {
+    font-size: 0.75rem;
+    line-height: 1rem;
+  }
+  color: #B0B0B0;
+`
+
+const AudioCard = ({ audioData, listViewSelected }) => {
+  const AudioCardContainer = listViewSelected
+    ? AudioCardListContainer
+    : AudioCardGridContainer
+  const PictureContainer = listViewSelected
+    ? ListPictureContainer
+    : GridPictureContainer
+  const SongTitle = listViewSelected
+    ? ListSongTitle
+    : GridSongTitle
+  const ArtistName = listViewSelected
+    ? ListArtistName
+    : GridArtistName
+  const AudioCardBlock = listViewSelected
+    ? ListAudioCardBlock
+    : GridAudioCardBlock
+  const AudioDataNumber = listViewSelected
+    ? ListAudioDataNumber
+    : GridAudioDataNumber
+  const AudioDurationContainer = listViewSelected
+    ? ListAudioDurationContainer
+    : GridAudioDurationContainer
+  const AudioData = listViewSelected
+    ? ListAudioData
+    : GridAudioData
+  const GenreTag = listViewSelected
+    ? ListGenreTag
+    : GridGenreTag
+
   return (
     <AudioCardContainer>
       <div>
@@ -139,24 +276,24 @@ const AudioCard = ({ audioData }) => {
           </div>
           <AudioDataContainer>
             <AudioData>
-              <FontAwesomeIcon icon={faCompactDisc} />
-              <AudioDataNumber>Like#</AudioDataNumber>
+              <FontAwesomeIcon icon={faHeart} />
+              <AudioDataNumber>1337</AudioDataNumber>
             </AudioData>
             <AudioData>
-              <FontAwesomeIcon icon={faCompactDisc} />
-              <AudioDataNumber>Listen#</AudioDataNumber>
+              <FontAwesomeIcon icon={faHeadphones} />
+              <AudioDataNumber>42</AudioDataNumber>
             </AudioData>
             <AudioData>
-              <FontAwesomeIcon icon={faCompactDisc} />
-              <AudioDataNumber>DL#</AudioDataNumber>
+              <FontAwesomeIcon icon={faDownload} />
+              <AudioDataNumber>314</AudioDataNumber>
             </AudioData>
           </AudioDataContainer>
         </AudioInformation>
         <AudioControls>
-          <AudioDurationContainer>13:37</AudioDurationContainer>
           <AudioControlButtonContainer>
-            <FontAwesomeIcon icon={faCompactDisc} />
+            <FontAwesomeIcon icon={faPlay} />
           </AudioControlButtonContainer>
+          <AudioDurationContainer>13:37</AudioDurationContainer>
         </AudioControls>
       </AudioCardBlock>
     </AudioCardContainer>
@@ -164,7 +301,8 @@ const AudioCard = ({ audioData }) => {
 }
 
 AudioCard.propTypes = {
-  audioData: PropTypes.object
+  audioData: PropTypes.object,
+  listViewSelected: PropTypes.bool
 }
 
 export default AudioCard
